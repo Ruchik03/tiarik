482b7050a203f924648d9be75e69b8abc7db9c0b
Ruchik03 <63090507+Ruchik03@users.noreply.github.com>
2021-01-16 13:29:48 -0800

    Update Message.py

--- Message.py
+++ Message.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python3
-#
-# Step two in building the messageboard server:
-# A server that handles both GET and POST requests.
-#
-# Instructions:
-#
-# 1. Add a string variable that contains the form from Messageboard.html.
-# 2. Add a do_GET method that returns the form.
-#
-# You don't need to change the do_POST method in this exercise!
-#
-# To test your code, run this server and access it at http://localhost:8000/
-# in your browser.  You should see the form.  Then put a message into the
-# form and submit it.  You should then see the message echoed back to you.
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
