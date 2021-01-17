a833ccb5aef2c91006f8c8a5ab2aa97068d48e32
RutujaD182 <dhanawaderutuja1002@gmail.com>
2021-01-17 18:32:01 +0530

    added post

--- flask-app-tiarik/app/views.py
+++ flask-app-tiarik/app/views.py
@@ -1,14 +1,25 @@
 # views.py
 
-from flask import render_template
+from flask import Flask, redirect, url_for, request, render_template
 
 from app import app
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        feelings = request.form
+        print(feelings)
+        return redirect(url_for('about', feelings=feelings))
+    else:
+        return render_template("index.html")
 
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template("about.html")
+
+
+@app.route('/result')
+def result():
+    return render_template("result.html")
